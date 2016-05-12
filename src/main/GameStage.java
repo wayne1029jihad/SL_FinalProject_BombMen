14aabe307438c1228860856940140a80ac87620b
Yang-Ming-Wang <r910214@yahoo.com.tw>
2016-05-12 23:21:00 +0800

    Create the framework of the game
    1. Add the new class 'GameStage'.
    2. The game will use processing to develop, so developer need to
    import some external jar which is provided from TA's Lab.

---
+++ src/main/GameStage.java
@@ -0,0 +1,18 @@
+package main;
+
+import processing.core.PApplet;
+/**
+* This class is for sketching outcome using Processing
+* You can do major UI control and some visualization in this class.
+*/
+@SuppressWarnings("serial")
+public class GameStage extends PApplet{
+	private final static int width = 1200, height = 650;
+	public void setup() {
+		size(width, height);
+		smooth();
+	}
+	public void draw() {
+		
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,31 @@
 package main;
 
-public class Main {
-	public static void main(String []args){
-		System.out.println("hello world");
+import javax.swing.JFrame;
+
+/**
+* This class is the main class of the program.
+* You can customize different window width and height for your program here. 
+*
+* @author  Richo
+* @version 1.0
+* @since   2016-04-26 
+*/
+@SuppressWarnings("serial")
+public class Main extends JFrame{
+	
+	private final static int windowWidth = 1200, windowHeight = 670;
+	
+	public static void main(String [] args){
+		
+		GameStage game = new GameStage();
+		game.init();
+		game.start();
+		game.setFocusable(true);
+		
+		JFrame window = new JFrame("BombMen");
+		window.setContentPane(game);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(windowWidth, windowHeight);
+		window.setVisible(true);
 	}
 }
